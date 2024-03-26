4898ec4af807eca3e87c901988647d2315d59e70
Goutam <goutambyte@gmail.com>
2024-03-26 22:31:23 +0530

    Structred response in controller

--- src/main/java/com/sample/microservice/controller/QuestionController.java
+++ src/main/java/com/sample/microservice/controller/QuestionController.java
@@ -1,29 +1,39 @@
 package com.sample.microservice.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sample.microservice.dto.QuestionDTO;
 import com.sample.microservice.dto.RestServiceResponse;
 
 @RestController
+@RequestMapping("/api/v1/quetion")
 public class QuestionController {
+
+	@GetMapping("/{id}")
 	public ResponseEntity<RestServiceResponse> getQuestionByID(@PathVariable String id) {
-		return null;
+		return ResponseEntity.ok().body(getSuccessResponse("Fetched succesfully"));
 	}
 
+	@DeleteMapping("/{id}")
 	public ResponseEntity<RestServiceResponse> deleteQuestion(@PathVariable String id) {
-		return null;
+		return ResponseEntity.ok().body(getSuccessResponse("Deleted succesfully"));
 	}
 
+	@PostMapping(consumes = "application/json")
 	public ResponseEntity<RestServiceResponse> saveQuestion(@RequestBody QuestionDTO questionDTO) {
-		return null;
+		return ResponseEntity.ok().body(getSuccessResponse("Saved succesfully"));
 	}
 
+	@PostMapping(value = "/search", consumes = "application/json")
 	public ResponseEntity<RestServiceResponse> searchQuestions() {
-		return null;
+		return ResponseEntity.ok().body(getSuccessResponse("search results returned"));
 	}
 
 }
