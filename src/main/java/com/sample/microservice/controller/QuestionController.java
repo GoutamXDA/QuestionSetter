7b701f6a30015004a7a57b9a37bfe28fd0d9c051
Goutam <goutambyte@gmail.com>
2024-03-26 21:34:37 +0530

    enchacement in controller

--- src/main/java/com/sample/microservice/controller/QuestionController.java
+++ src/main/java/com/sample/microservice/controller/QuestionController.java
@@ -1,12 +1,29 @@
 package com.sample.microservice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.sample.microservice.dto.QuestionDTO;
+import com.sample.microservice.dto.RestServiceResponse;
 
 @RestController
 public class QuestionController {
-	@GetMapping("/api/v1/greeting")
-	public String greeting() {
-		return "Hello World";
+	public ResponseEntity<RestServiceResponse> getQuestionByID(@PathVariable String id) {
+		return null;
 	}
+
+	public ResponseEntity<RestServiceResponse> deleteQuestion(@PathVariable String id) {
+		return null;
+	}
+
+	public ResponseEntity<RestServiceResponse> saveQuestion(@RequestBody QuestionDTO questionDTO) {
+		return null;
+	}
+
+	public ResponseEntity<RestServiceResponse> searchQuestions() {
+		return null;
+	}
+
 }
